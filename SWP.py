b434fb9dff8c4db0dc6e21bb0569d15486c65a2b
Angmtlln <136786363+Angmtlln@users.noreply.github.com>
2023-07-02 22:31:06 +0300

    Update SWP.py
    Added emulation registration of users

--- SWP.py
+++ SWP.py
@@ -2,28 +2,22 @@ import telebot
 from telebot import types
 
 
-bot = telebot.TeleBot('6155094584:AAE5isPBEmZ1quEGKICR2h5y7bZ9YGhN2sM')
+bot = telebot.TeleBot('')
 
 emoji = {'correct': '\U00002705', 'wrong': '\U0000274C', 'back': '\U00002B05', }
 
 
 @bot.message_handler(commands=['start'])
 def bot_start(message: types.Message):
-    keyboard = types.InlineKeyboardMarkup()
     markup1 = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
     button1 = types.KeyboardButton("About")
     button2 = types.KeyboardButton("Help")
     button3 = types.KeyboardButton("Settings")
     markup1.add(button1, button2, button3)
     url_button = types.InlineKeyboardButton(text="Авторизоваться на сайте", url="https://sport.innopolis.university")
-    keyboard.add(url_button)
     bot.send_message(message.chat.id,
-                     text='\tWelcome to InnoSportChallengesBot.',
+                     text='\tWelcome to InnoSportChallengesBot. To register you on Innopolis Sport cite, please write your email',
                      reply_markup=markup1)
-
-    bot.send_message(message.chat.id,
-                     text='To register your account, please follow the link',
-                     reply_markup=keyboard)
 
 
 @bot.message_handler(content_types=['text'])
@@ -53,35 +47,48 @@ def Buttom(message):
         bot.send_message(message.chat.id, "Turn the Notifications ON or OFF?", reply_markup=markup2)
 
     elif message.text == f'{emoji["wrong"]} OFF':
-        keyboard = types.InlineKeyboardMarkup()
-        url_button = types.InlineKeyboardButton(text="Авторизоваться на сайте",
-                                                url="https://sport.innopolis.university")
-        keyboard.add(url_button)
-        bot.send_message(message.chat.id, "Notifications are OFF")
-        markup2 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-        button4 = types.KeyboardButton(text=f'{emoji["correct"]} ON')
-        button5 = types.KeyboardButton(text=f'{emoji["wrong"]} OFF')
-        button6 = types.KeyboardButton(text=f'{emoji["back"]} Back')
-        markup2.add(button4, button5, button6)
-        bot.send_message(message.chat.id, "Turn the Notifications ON or OFF?", reply_markup=markup2)
-        bot.send_message(message.chat.id,
-                         text='To register your account, please follow the link',
-                         reply_markup=keyboard)
-
-    elif message.text == f'{emoji["back"]} Back':
-        keyboard = types.InlineKeyboardMarkup()
-        url_button = types.InlineKeyboardButton(text="Авторизоваться на сайте",
-                                                url="https://sport.innopolis.university")
-        keyboard.add(url_button)
         markup1 = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
         button1 = types.KeyboardButton("About")
         button2 = types.KeyboardButton("Help")
         button3 = types.KeyboardButton("Settings")
         markup1.add(button1, button2, button3)
-        bot.send_message(message.chat.id, text='\tWelcome to InnoSportChallengesBot.', reply_markup=markup1)
+        url_button = types.InlineKeyboardButton(text="Авторизоваться на сайте",
+                                                url="https://sport.innopolis.university")
         bot.send_message(message.chat.id,
-                         text='To register your account, please follow the link',
-                         reply_markup=keyboard)
-        
+                         text='\tWelcome to InnoSportChallengesBot. To register you on Innopolis Sport cite, please write your email',
+                         reply_markup=markup1)
 
+    elif message.text == f'{emoji["back"]} Back':
+        markup1 = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+        button1 = types.KeyboardButton("About")
+        button2 = types.KeyboardButton("Help")
+        button3 = types.KeyboardButton("Settings")
+        markup1.add(button1, button2, button3)
+        url_button = types.InlineKeyboardButton(text="Авторизоваться на сайте",
+                                                url="https://sport.innopolis.university")
+        bot.send_message(message.chat.id,
+                         text='\tWelcome to InnoSportChallengesBot. To register you on Innopolis Sport cite, please write your email',
+                         reply_markup=markup1)
+    elif "@innopolis.university" in message.text:
+        markup1 = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+        button1 = types.KeyboardButton("About")
+        button2 = types.KeyboardButton("Help")
+        button3 = types.KeyboardButton("Settings")
+        markup1.add(button1, button2, button3)
+        url_button = types.InlineKeyboardButton(text="Авторизоваться на сайте",
+                                                url="https://sport.innopolis.university")
+        bot.send_message(message.chat.id,
+                         text='\tPlease write your password',
+                         reply_markup=markup1)
+    else:
+        markup1 = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+        button1 = types.KeyboardButton("About")
+        button2 = types.KeyboardButton("Help")
+        button3 = types.KeyboardButton("Settings")
+        markup1.add(button1, button2, button3)
+        url_button = types.InlineKeyboardButton(text="Авторизоваться на сайте",
+                                                url="https://sport.innopolis.university")
+        bot.send_message(message.chat.id,
+                         text='\tYour account has been successfully registered',
+                         reply_markup=markup1)
 bot.polling(none_stop=True, interval=0)
